0c5a92581606cce76992e75599e4d1501bca103f
Teo <teodor_paius@yahoo.com>
2019-03-18 17:14:04 +0200

    added test casse
    a

--- src/test/java/lab02v2MV/AppTest.java
+++ src/test/java/lab02v2MV/AppTest.java
@@ -2,7 +2,18 @@ package lab02v2MV;
 
 import static org.junit.Assert.assertTrue;
 
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
 import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
 
 /**
  * Unit test for simple App.
@@ -17,4 +28,29 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+    public void addStudentTest() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.saveStudent("100","test",935);
+        boolean found = false;
+        for (Student s:
+             service.findAllStudents()) {
+            if(s.getID().equals("100"))
+            {
+                found = true;
+            }
+        }
+        assertTrue(found);
+
+    }
+
 }
